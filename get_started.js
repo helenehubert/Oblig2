ab5e4a924182b65ea1c4eea88d0e382725eb578d
helenehubert <165794354+helenehubert@users.noreply.github.com>
2024-04-03 09:40:25 +0200

    Add files via upload

--- get_started.js
+++ get_started.js
@@ -5,6 +5,25 @@ function submitForm() {
     
 }
 
+const url = 'http://localhost:8080/submit-form'; // URL to our server endpoint
+const requestData = {
+  variable: statisticVariable // Variable to be passed to the server
+};
+fetch(url, { //use of fetch method to send variable from frontend to backend
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(requestData)
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('Response from server:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
 function displayResults(results) {
     const table = document.getElementById('resultsTable');
     const tbody = table.getElementsByTagName('tbody')[0];
